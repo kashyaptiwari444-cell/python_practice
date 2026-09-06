09e0b697964966c5a91608f358791dadaff3de7d
Kashyap Tiwari <kashyaptiwari444@gmail.com>
2026-09-06 15:45:53 +0000

    name change for future

--- 08_factorial.py
+++ 08_factorial.py
@@ -1,3 +1,5 @@
+# 5! = 1 × 2 × 3 × 4 × 5 = 120
+
 num = int(input("Enter the number: "))
 fact = 1
 for i in range(1, num+1):
---
+++ 10_fibonacci.py
@@ -0,0 +1,4 @@
+num = int(input("Enter Number: "))
+feb = 1
+for i in range(0,feb):
+    

9462df6062a07082d209e0fa800854a927e42463
Kashyap Tiwari <kashyaptiwari444@gmail.com>
2026-09-12 16:42:20 +0000

    start code 50

--- 10_fibonacci.py
+++ 10_fibonacci.py
@@ -1,4 +1,8 @@
+# Input n = 5: Output is 5 (sequence: 0, 1, 1, 2, 3, 5)
+# Input n = 6: Output is 8 (sequence: 0, 1, 1, 2, 3, 5, 8)
+
 num = int(input("Enter Number: "))
-feb = 1
-for i in range(0,feb):
+feb = 0
+while True:
+    if 
     

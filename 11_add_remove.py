c7da502136a294aacad05d2f390252c170032122
Kashyap Tiwari <kashyaptiwari444@gmail.com>
2026-09-16 17:06:42 +0000

    delete specific element and last elemrnt in a li

--- 11_add_remove.py
+++ 11_add_remove.py
@@ -1,10 +1,20 @@
+# add element in a list
+
 data = ["jack", "raj", "om", "spider", "david"]
 print(data)
-data.append("rokey")
-print(data)
+# data.append("rokey")
+# print(data)    # ['jack', 'raj', 'om', 'spider', 'david', 'rokey']
 
-# ['jack', 'raj', 'om', 'spider', 'david']
-# ['jack', 'raj', 'om', 'spider', 'david', 'rokey']
+
+#===============================================================
+
+# Delete specific
+# data.remove("raj")
+# print(data)    # ['jack', 'om', 'spider', 'david']
+
+# Delete last element
+# data.pop()
+# print(data)    # ['jack', 'om', 'spider', 'david']
 
 #===============================================================
 
